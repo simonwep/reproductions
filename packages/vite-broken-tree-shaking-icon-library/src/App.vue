688908636c33a211f8f0052b550c81d76d8ea8ec
Simon Reinisch <simon@reinisch.io>
2026-06-19 17:23:44 +0200

    Add repro for broken tree-shaking in vite when using dynamic imports

---
+++ packages/vite-broken-tree-shaking-icon-library/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  Hello world!
+
+  <!-- Uncomment for repro -->
+  <!-- <Icons/> -->
+</template>
+
+<script setup lang="ts">
+// Uncomment for repro
+// This way the library used in Icons.vue is properly tree-shaked
+// import Icons from "./Icons.vue";
+
+// Comment out for repro
+// This way the whole library is bundled and NOT tree-shaked
+void await import('./Icons.vue');
+</script>
