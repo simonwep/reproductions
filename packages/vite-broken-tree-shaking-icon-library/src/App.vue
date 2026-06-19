2116f5c6f1f4795d7b94a355f47b89b475b3845d
Simon Reinisch <simon@reinisch.io>
2026-06-19 17:33:15 +0200

    Simplify tree-shaking repro

--- packages/vite-broken-tree-shaking-icon-library/src/App.vue
+++ packages/vite-broken-tree-shaking-icon-library/src/App.vue
@@ -8,9 +8,9 @@
 <script setup lang="ts">
 // Uncomment for repro
 // This way the library used in Icons.vue is properly tree-shaked
-// import Icons from "./Icons.vue";
+// import Icons from './Icons.vue';
 
 // Comment out for repro
 // This way the whole library is bundled and NOT tree-shaked
-void await import('./Icons.vue');
+void import('./Icons.vue');
 </script>
